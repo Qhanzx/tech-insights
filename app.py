4954fee69b905bc14973c2552c05be9d22535eb5
Qhanzx <125101628+Qhanzx@users.noreply.github.com>
2023-11-11 16:09:07 +0000

    Added dynamic data and api route

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+TROVES = [{
+    'id': 1,
+    'title': 'Java',
+    'content': 'This site offers a vast selection of Java Essentials',
+    'author': 'Matt'
+}, {
+    'id': 2,
+    'title': 'Python',
+    'content': 'This site offers a vast selection of Python Essentials',
+    'author': 'Sam'
+}, {
+    'id': 1,
+    'title': 'Cisco',
+    'content': 'This site offers a vast selection of Cisco Essentials',
+    'author': 'Vahid'
+}]
+
+@app.route('/')
+def index():
+  return render_template('index.html', posts=TROVES, tech='Qhanzx')
+
+
+@app.route('/api/posts')
+def troves():
+  return jsonify(TROVES)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
